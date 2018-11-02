8db78d2bc4c7372a696cd6b0538441ac1d2df670
dukecg@gmail.com <dukecg@gmail.com>
2018-11-02 18:46:57 +1100

    4.20 compat

--- Source/UMGPlus/UMGPlus.Build.cs
+++ Source/UMGPlus/UMGPlus.Build.cs
@@ -5,22 +5,25 @@ public class UMGPlus : ModuleRules
 	public UMGPlus(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
-		
-		PublicIncludePaths.AddRange(
-			new string[] {
-				"UMGPlus/Public",
-			    "UMGPlus/Public/Components",
-                "UMGPlus/Public/Interfaces",
-			    "UMGPlus/Public/Widgets"
-            });
 
-		PrivateIncludePaths.AddRange(
-			new string[] {
-				"UMGPlus/Private",
-			    "UMGPlus/Private/Components",
-                "UMGPlus/Private/Interfaces",
-			    "UMGPlus/Private/Widgets"
-            });
+	    if (Target.Version.MinorVersion <= 19)
+	    {
+            PublicIncludePaths.AddRange(
+			    new string[] {
+				    "UMGPlus/Public",
+			        "UMGPlus/Public/Components",
+                    "UMGPlus/Public/Interfaces",
+			        "UMGPlus/Public/Widgets"
+                });
+
+		    PrivateIncludePaths.AddRange(
+			    new string[] {
+				    "UMGPlus/Private",
+			        "UMGPlus/Private/Components",
+                    "UMGPlus/Private/Interfaces",
+			        "UMGPlus/Private/Widgets"
+                });
+        }
 
 		PublicDependencyModuleNames.AddRange(
 			new string[]
@@ -42,7 +45,5 @@ public class UMGPlus : ModuleRules
 				"SlateCore",
                 "UMG"
 			});
-
-		DynamicallyLoadedModuleNames.AddRange(new string[] { });
 	}
 }
